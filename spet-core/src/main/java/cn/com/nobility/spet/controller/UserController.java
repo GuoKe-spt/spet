e106315b6298050a76f90b85b51b22d37123480a
sunpengtao <GuoKely@126.com>
2019-05-15 11:56:21 +0800

    模块名修改

--- spet-core/src/main/java/cn/com/nobility/spet/controller/UserController.java
+++ spet-core/src/main/java/cn/com/nobility/spet/controller/UserController.java
@@ -1,9 +1,13 @@
 package cn.com.nobility.spet.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author: spt
  * @description:
  * @date: 2019/5/15 11:41
  */
+@RestController
 public class UserController {
+
 }
