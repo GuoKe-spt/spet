4adcc20f36fa20b348e3d26f7a702f18ddf68460
sunpengtao <GuoKely@126.com>
2019-05-15 11:38:47 +0800

    Controller模块添加

--- spet-po/src/main/java/cn/com/nobility/po/User.java
+++ spet-po/src/main/java/cn/com/nobility/po/User.java
@@ -3,6 +3,9 @@ package cn.com.nobility.po;
 
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 /**
  * @author: spt
  * @description:
@@ -11,13 +14,26 @@ import lombok.Data;
 @Data
 public class User {
 
+    @NotBlank(message = "主键不能为空")
     private String id;
+
+    @NotBlank(message = "姓名不能为空")
     private String name;
+
+    @NotBlank(message = "用户名不能为空")
     private String userName;
+
+    @NotBlank(message = "密码不能为空")
     private String passWord;
+
     private String phone;
+    @Email(message = "邮箱格式不正确")
     private String email;
+
     private String sex;
-    private String address;
+
     private Integer age;
+
+    private String address;
+
 }
